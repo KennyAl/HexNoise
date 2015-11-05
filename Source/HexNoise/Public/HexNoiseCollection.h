acdec56e34034935e7b7a980a665e9054248b2a1
KennyA <KennyA@kabelmail.de>
2015-11-05 22:49:11 +0100

    Added a seperate header for all includes

---
+++ Source/HexNoise/Public/HexNoiseCollection.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "FPerlinNoiseGenerator1D.h"
+#include "FPerlinNoiseGenerator2D.h"
+#include "FPerlinNoiseGenerator3D.h"
+#include "Settings.h"
+
+#include "FSubstract.h"
+#include "FAdd.h"
+#include "FMultiply.h"
+#include "FConstant.h"
